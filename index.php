b725baf364d556d3bf38db2c11a4cdd246fde16a
srph <seraphipod@gmail.com>
2015-12-12 14:45:01 +0800

    Remove example file.

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-require 'Qp/Qp.php';
-require 'Qp/Stringifier.php';
-require 'Qp/Parser.php';
-use Qp\Qp;
-
-print_r(Qp::parse('user[name]=kier'));
-print_r(Qp::parse('users[]=kier'));
-print_r(Qp::stringify(['yolo' => 'swag', 'swag' => 'yolo']));
